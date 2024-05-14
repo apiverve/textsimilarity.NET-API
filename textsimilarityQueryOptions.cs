97728ec222feed0864f73c7a9733cfd78cf1f0c5
APIVerve Team <hello@apiverve.com>
2024-05-14 15:43:13 -0500

    Bug fixes and Improvements

---
+++ textsimilarityQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class textsimilarityQueryOptions {
+public string text1 { get; set; }
+public string text2 { get; set; }
+}
+
+}
