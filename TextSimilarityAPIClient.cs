525ea9cff9de2ad52557810c7afaa6f6e1d3600f
APIVerve Team <hello@apiverve.com>
2024-08-20 11:10:30 -0500

    Bug Fixes

--- TextSimilarityAPIClient.cs
+++ TextSimilarityAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, textsimilarityQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, TextSimilarityQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(textsimilarityQueryOptions options = null)
+        public ResponseObj Execute(TextSimilarityQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(textsimilarityQueryOptions options)
+        private string constructURL(TextSimilarityQueryOptions options)
         {
             string url = _apiEndpoint;
 
